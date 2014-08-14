3a0c81b98120eff4f32511a20966adb82e6366dd
Dave Cowart <davecowart@gmail.com>
2014-08-14 17:35:04 -0500

    add check in web.config for any specified mime types

---
+++ N2.S3Filesystem/MimeExtractor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using Microsoft.Web.Administration;
+
+namespace N2.S3FileSystem {
+  internal class MimeExtractor {
+    private const string CacheKey = "mime_types_cache";
+    private const string CacheDictionaryKey = "mime_types_cache_dictionary";
+
+    public string GetMimeTypeForExtension(string extension) {
+      if (HttpContext.Current.Cache[CacheKey] == null) {
+        HttpContext.Current.Cache.Insert(CacheDictionaryKey, ParseMimeTypes());
+      }
+      if (!extension.StartsWith(".")) {
+        extension = "." + extension;
+      }
+
+      var mimeDictionary = (IDictionary<string, string>)HttpContext.Current.Cache[CacheDictionaryKey];
+      return mimeDictionary.ContainsKey(extension) ? mimeDictionary[extension] : null;
+    }
+
+    private static IDictionary<string, string> ParseMimeTypes() {
+      using (var serverManager = new ServerManager()) {
+        var siteName = HostingEnvironment.ApplicationHost.GetSiteName();
+        var config = serverManager.GetWebConfiguration(siteName);
+        var staticContentSection = config.GetSection("system.webServer/staticContent");
+        var staticContentCollection = staticContentSection.GetCollection();
+
+        var mimeMaps = staticContentCollection.Where(c =>
+          c.ElementTagName == "mimeMap"
+          && c.GetAttributeValue("fileExtension") != null
+          && !string.IsNullOrWhiteSpace(c.GetAttributeValue("fileExtension").ToString())
+          && c.GetAttributeValue("mimeType") != null
+          && !string.IsNullOrWhiteSpace(c.GetAttributeValue("mimeType").ToString())
+        );
+        var results = mimeMaps.Select(m => new KeyValuePair<string, string>(m.GetAttributeValue("fileExtension").ToString(), m.GetAttributeValue("mimeType").ToString()));
+        return results.ToDictionary(pair => pair.Key, pair => pair.Value);
+      }
+    }
+  }
+}
--- N2.S3Filesystem/S3FileSystem.cs
+++ N2.S3Filesystem/S3FileSystem.cs
@@ -10,255 +10,260 @@ using N2.Engine;
 
 // ReSharper disable CheckNamespace
 namespace N2.Edit.FileSystem {
-// ReSharper restore CheckNamespace 
-	[Service(typeof(IFileSystem))]
-	public class S3FileSystem : IFileSystem {
-		private readonly string _accessKeyId;
-		private readonly string _secretAccessKey;
-		private readonly AmazonS3 _s3;
-		private readonly string _bucketName;
-		private const string RootURL = @"https://s3.amazonaws.com/{0}/{1}";
-		private const string EmptyFilename = @"__empty";
+  // ReSharper restore CheckNamespace 
+  [Service(typeof(IFileSystem))]
+  public class S3FileSystem : IFileSystem {
+    private readonly string _accessKeyId;
+    private readonly string _secretAccessKey;
+    private readonly AmazonS3 _s3;
+    private readonly string _bucketName;
+    private const string RootURL = @"https://s3.amazonaws.com/{0}/{1}";
+    private const string EmptyFilename = @"__empty";
 
-		public S3FileSystem() {
-			_accessKeyId = ConfigurationManager.AppSettings["AWSAccessKeyID"];
-			_secretAccessKey = ConfigurationManager.AppSettings["AWSSecretAccessKey"];
-			_bucketName = ConfigurationManager.AppSettings["AWSBucketName"];
-			_s3 = AWSClientFactory.CreateAmazonS3Client(_accessKeyId, _secretAccessKey);
-		}
+    public S3FileSystem() {
+      _accessKeyId = ConfigurationManager.AppSettings["AWSAccessKeyID"];
+      _secretAccessKey = ConfigurationManager.AppSettings["AWSSecretAccessKey"];
+      _bucketName = ConfigurationManager.AppSettings["AWSBucketName"];
+      _s3 = AWSClientFactory.CreateAmazonS3Client(_accessKeyId, _secretAccessKey);
+    }
 
-		#region Implementation of IFileSystem
+    #region Implementation of IFileSystem
 
-		public IEnumerable<FileData> GetFiles(string parentVirtualPath) {
-			var request = new ListObjectsRequest()
-				.WithBucketName(_bucketName)
-				.WithPrefix(FixVirtualPath(parentVirtualPath))
-				.WithDelimiter(@"/");
+    public IEnumerable<FileData> GetFiles(string parentVirtualPath) {
+      var request = new ListObjectsRequest()
+        .WithBucketName(_bucketName)
+        .WithPrefix(FixVirtualPath(parentVirtualPath))
+        .WithDelimiter(@"/");
 
-			IEnumerable<FileData> files;
-			using (var response = _s3.ListObjects(request)) {
-				files = response.S3Objects.Where(file => file.Size > 0).Select(file => new FileData {
-					VirtualPath = file.Key,
-					Name = file.Key.Substring(file.Key.LastIndexOf('/') + 1),
-					Length = file.Size,
-					Created = DateTime.Parse(file.LastModified),
-					Updated = DateTime.Parse(file.LastModified)
-				});
-			}
-			return files;
-		}
+      IEnumerable<FileData> files;
+      using (var response = _s3.ListObjects(request)) {
+        files = response.S3Objects.Where(file => file.Size > 0).Select(file => new FileData {
+          VirtualPath = file.Key,
+          Name = file.Key.Substring(file.Key.LastIndexOf('/') + 1),
+          Length = file.Size,
+          Created = DateTime.Parse(file.LastModified),
+          Updated = DateTime.Parse(file.LastModified)
+        });
+      }
+      return files;
+    }
 
-		public FileData GetFile(string virtualPath) {
-			var request = new GetObjectRequest()
-				.WithBucketName(_bucketName)
-				.WithKey(FixVirtualPath(virtualPath));
-			FileData file;
-			using (var response = _s3.GetObject(request)) {
-				file = new FileData {
-					Name = response.Key.Substring(response.Key.LastIndexOf('/') + 1),
-					Updated = DateTime.Now,
-					Created = DateTime.Now,
-					Length = response.ContentLength,
-					VirtualPath = response.Key
-				};
-			}
-			return file;
-		}
+    public FileData GetFile(string virtualPath) {
+      var request = new GetObjectRequest()
+        .WithBucketName(_bucketName)
+        .WithKey(FixVirtualPath(virtualPath));
+      FileData file;
+      using (var response = _s3.GetObject(request)) {
+        file = new FileData {
+          Name = response.Key.Substring(response.Key.LastIndexOf('/') + 1),
+          Updated = DateTime.Now,
+          Created = DateTime.Now,
+          Length = response.ContentLength,
+          VirtualPath = response.Key
+        };
+      }
+      return file;
+    }
 
-		public IEnumerable<DirectoryData> GetDirectories(string parentVirtualPath) {
-			parentVirtualPath = FixVirtualPath(parentVirtualPath);
+    public IEnumerable<DirectoryData> GetDirectories(string parentVirtualPath) {
+      parentVirtualPath = FixVirtualPath(parentVirtualPath);
 
-			var request = new ListObjectsRequest()
-				.WithBucketName(_bucketName)
-				.WithPrefix(parentVirtualPath)
-				.WithDelimiter(@"/");
+      var request = new ListObjectsRequest()
+        .WithBucketName(_bucketName)
+        .WithPrefix(parentVirtualPath)
+        .WithDelimiter(@"/");
 
-			IEnumerable<DirectoryData> directories;
-			using (var response = _s3.ListObjects(request)) {
-				directories = response.CommonPrefixes.Select(dir => new DirectoryData {
-					Created = DateTime.Now,
-					Updated = DateTime.Now,
-					Name = dir.TrimEnd('/').Substring(dir.TrimEnd('/').LastIndexOf('/') + 1),
-					VirtualPath = dir
-				});
-			}
-			return directories;
-		}
+      IEnumerable<DirectoryData> directories;
+      using (var response = _s3.ListObjects(request)) {
+        directories = response.CommonPrefixes.Select(dir => new DirectoryData {
+          Created = DateTime.Now,
+          Updated = DateTime.Now,
+          Name = dir.TrimEnd('/').Substring(dir.TrimEnd('/').LastIndexOf('/') + 1),
+          VirtualPath = dir
+        });
+      }
+      return directories;
+    }
 
-		public DirectoryData GetDirectory(string virtualPath) {
-			virtualPath = FixVirtualPath(virtualPath);
-			return new DirectoryData {
-				Name = string.Format(RootURL, _bucketName, virtualPath),
-				VirtualPath = virtualPath,
-				Created = DateTime.Now,
-				Updated = DateTime.Now
-			};
-		}
+    public DirectoryData GetDirectory(string virtualPath) {
+      virtualPath = FixVirtualPath(virtualPath);
+      return new DirectoryData {
+        Name = string.Format(RootURL, _bucketName, virtualPath),
+        VirtualPath = virtualPath,
+        Created = DateTime.Now,
+        Updated = DateTime.Now
+      };
+    }
 
-		public bool FileExists(string virtualPath) {
-			var request = new GetObjectMetadataRequest()
-				.WithBucketName(_bucketName)
-				.WithKey(FixVirtualPath(virtualPath));
+    public bool FileExists(string virtualPath) {
+      var request = new GetObjectMetadataRequest()
+        .WithBucketName(_bucketName)
+        .WithKey(FixVirtualPath(virtualPath));
 
-			try {
-				using (_s3.GetObjectMetadata(request)) { }
-			} catch (AmazonS3Exception) {
-				return false;
-			}
-			return true;
-		}
+      try {
+        using (_s3.GetObjectMetadata(request)) { }
+      } catch (AmazonS3Exception) {
+        return false;
+      }
+      return true;
+    }
 
-		public void MoveFile(string fromVirtualPath, string destinationVirtualPath) {
-			CopyFile(fromVirtualPath, destinationVirtualPath);
-			DeleteFile(fromVirtualPath);
-		}
+    public void MoveFile(string fromVirtualPath, string destinationVirtualPath) {
+      CopyFile(fromVirtualPath, destinationVirtualPath);
+      DeleteFile(fromVirtualPath);
+    }
 
-		public void DeleteFile(string virtualPath) {
-			var request = new DeleteObjectRequest()
-				.WithBucketName(_bucketName)
-				.WithKey(FixVirtualPath(virtualPath));
+    public void DeleteFile(string virtualPath) {
+      var request = new DeleteObjectRequest()
+        .WithBucketName(_bucketName)
+        .WithKey(FixVirtualPath(virtualPath));
 
-			using (_s3.DeleteObject(request)) { }
+      using (_s3.DeleteObject(request)) { }
 
-			if (FileDeleted != null)
-				FileDeleted.Invoke(this, new FileEventArgs(virtualPath, null));
-		}
+      if (FileDeleted != null)
+        FileDeleted.Invoke(this, new FileEventArgs(virtualPath, null));
+    }
 
-		public void CopyFile(string fromVirtualPath, string destinationVirtualPath) {
-			var copyRequest = new CopyObjectRequest()
+    public void CopyFile(string fromVirtualPath, string destinationVirtualPath) {
+      var copyRequest = new CopyObjectRequest()
         .WithMetaData("Expires", DateTime.Now.AddYears(10).ToString("R"))
-				.WithSourceBucket(_bucketName)
-				.WithSourceKey(fromVirtualPath)
-				.WithDestinationBucket(_bucketName)
-				.WithDestinationKey(destinationVirtualPath)
-				.WithCannedACL(S3CannedACL.PublicRead);
+        .WithSourceBucket(_bucketName)
+        .WithSourceKey(fromVirtualPath)
+        .WithDestinationBucket(_bucketName)
+        .WithDestinationKey(destinationVirtualPath)
+        .WithCannedACL(S3CannedACL.PublicRead);
 
-			using (_s3.CopyObject(copyRequest)) { }
-		}
+      using (_s3.CopyObject(copyRequest)) { }
+    }
 
 
-		public Stream OpenFile(string virtualPath, bool readOnly = false) {
-			var request = new GetObjectRequest()
-				.WithBucketName(_bucketName)
-				.WithKey(FixVirtualPath(virtualPath));
+    public Stream OpenFile(string virtualPath, bool readOnly = false) {
+      var request = new GetObjectRequest()
+        .WithBucketName(_bucketName)
+        .WithKey(FixVirtualPath(virtualPath));
 
-			var stream = new MemoryStream();
+      var stream = new MemoryStream();
 
-			using (var response = _s3.GetObject(request)) {
-				var buffer = new byte[32768];
-				while (true) {
-					var read = response.ResponseStream.Read(buffer, 0, buffer.Length);
-					if (read <= 0)
-						break;
-					stream.Write(buffer, 0, read);
-				}
-			}
-			return stream;
-		}
+      using (var response = _s3.GetObject(request)) {
+        var buffer = new byte[32768];
+        while (true) {
+          var read = response.ResponseStream.Read(buffer, 0, buffer.Length);
+          if (read <= 0)
+            break;
+          stream.Write(buffer, 0, read);
+        }
+      }
+      return stream;
+    }
 
-		public void WriteFile(string virtualPath, Stream inputStream) {
-			var request = new PutObjectRequest()
+    public void WriteFile(string virtualPath, Stream inputStream) {
+      var request = new PutObjectRequest()
         .WithMetaData("Expires", DateTime.Now.AddYears(10).ToString("R"))
-				.WithBucketName(_bucketName)
-				.WithCannedACL(S3CannedACL.PublicRead)
+        .WithBucketName(_bucketName)
+        .WithCannedACL(S3CannedACL.PublicRead)
         .WithTimeout(60 * 60 * 1000) // 1 hour
         .WithReadWriteTimeout(60 * 60 * 1000) // 1 hour
-				.WithKey(FixVirtualPath(virtualPath));
+        .WithKey(FixVirtualPath(virtualPath));
 
-			request.WithInputStream(inputStream);
-			using (_s3.PutObject(request)) { }
-		}
+      var contentType = virtualPath.Substring(virtualPath.LastIndexOf(".", StringComparison.Ordinal));
+      if (string.IsNullOrWhiteSpace(contentType)) {
+        request.ContentType = contentType;
+      }
 
-		public void ReadFileContents(string virtualPath, Stream outputStream) {
-			var request = new GetObjectRequest()
-				.WithBucketName(_bucketName)
-				.WithKey(FixVirtualPath(virtualPath));
+      request.WithInputStream(inputStream);
+      using (_s3.PutObject(request)) { }
+    }
 
-			using (var response = _s3.GetObject(request)) {
-				var buffer = new byte[32768];
-				while (true) {
-					var read = response.ResponseStream.Read(buffer, 0, buffer.Length);
-					if (read <= 0) break;
-					outputStream.Write(buffer, 0, read);
-				}
-			}
-		}
+    public void ReadFileContents(string virtualPath, Stream outputStream) {
+      var request = new GetObjectRequest()
+        .WithBucketName(_bucketName)
+        .WithKey(FixVirtualPath(virtualPath));
 
-		public bool DirectoryExists(string virtualPath) { // ~/upload/28/
-			virtualPath = FixVirtualPath(virtualPath) + EmptyFilename;
+      using (var response = _s3.GetObject(request)) {
+        var buffer = new byte[32768];
+        while (true) {
+          var read = response.ResponseStream.Read(buffer, 0, buffer.Length);
+          if (read <= 0) break;
+          outputStream.Write(buffer, 0, read);
+        }
+      }
+    }
 
-			var request = new GetObjectMetadataRequest()
-				.WithBucketName(_bucketName)
-				.WithKey(virtualPath);
+    public bool DirectoryExists(string virtualPath) { // ~/upload/28/
+      virtualPath = FixVirtualPath(virtualPath) + EmptyFilename;
 
-			try {
-				using (_s3.GetObjectMetadata(request)) { }
-			} catch (AmazonS3Exception) {
-				return false;
-			}
-			return true;
-		}
+      var request = new GetObjectMetadataRequest()
+        .WithBucketName(_bucketName)
+        .WithKey(virtualPath);
 
-		public void MoveDirectory(string fromVirtualPath, string destinationVirtualPath) {
-			throw new NotImplementedException();
-		}
+      try {
+        using (_s3.GetObjectMetadata(request)) { }
+      } catch (AmazonS3Exception) {
+        return false;
+      }
+      return true;
+    }
 
-		public void DeleteDirectory(string virtualPath) { //upload/test/
-			virtualPath = FixVirtualPath(virtualPath);
-			DeleteDirectoryAndChildren(virtualPath);
-			if (DirectoryDeleted != null)
-				DirectoryDeleted.Invoke(this, new FileEventArgs(virtualPath, null));
-		}
+    public void MoveDirectory(string fromVirtualPath, string destinationVirtualPath) {
+      throw new NotImplementedException();
+    }
 
-		public void CreateDirectory(string virtualPath) {
-			virtualPath = string.Format("{0}/{1}", FixVirtualPath(virtualPath), EmptyFilename);
+    public void DeleteDirectory(string virtualPath) { //upload/test/
+      virtualPath = FixVirtualPath(virtualPath);
+      DeleteDirectoryAndChildren(virtualPath);
+      if (DirectoryDeleted != null)
+        DirectoryDeleted.Invoke(this, new FileEventArgs(virtualPath, null));
+    }
 
-			var request = new PutObjectRequest()
-				.WithBucketName(_bucketName)
-				.WithKey(virtualPath)
-				.WithContentBody(string.Empty)
-				.WithContentType("text");
-			using (_s3.PutObject(request)) { }
-		}
+    public void CreateDirectory(string virtualPath) {
+      virtualPath = string.Format("{0}/{1}", FixVirtualPath(virtualPath), EmptyFilename);
 
-		//TODO: implement these (need to figure out exactly what to set in FileEventArgs)
-		public event EventHandler<FileEventArgs> FileWritten;
-		public event EventHandler<FileEventArgs> FileCopied;
-		public event EventHandler<FileEventArgs> FileMoved;
-		public event EventHandler<FileEventArgs> FileDeleted;
-		public event EventHandler<FileEventArgs> DirectoryCreated;
-		public event EventHandler<FileEventArgs> DirectoryMoved;
-		public event EventHandler<FileEventArgs> DirectoryDeleted;
+      var request = new PutObjectRequest()
+        .WithBucketName(_bucketName)
+        .WithKey(virtualPath)
+        .WithContentBody(string.Empty)
+        .WithContentType("text");
+      using (_s3.PutObject(request)) { }
+    }
 
-		#endregion
+    //TODO: implement these (need to figure out exactly what to set in FileEventArgs)
+    public event EventHandler<FileEventArgs> FileWritten;
+    public event EventHandler<FileEventArgs> FileCopied;
+    public event EventHandler<FileEventArgs> FileMoved;
+    public event EventHandler<FileEventArgs> FileDeleted;
+    public event EventHandler<FileEventArgs> DirectoryCreated;
+    public event EventHandler<FileEventArgs> DirectoryMoved;
+    public event EventHandler<FileEventArgs> DirectoryDeleted;
 
-		private string FixVirtualPath(string virtualPath) {
-			if (virtualPath.StartsWith(@"~/"))
-				return virtualPath.Replace(@"~/", string.Empty).TrimStart('/');
+    #endregion
 
-			if (virtualPath.StartsWith(@"/N2/"))
-				return virtualPath.Replace(@"/N2/", string.Empty).TrimStart('/');
+    private string FixVirtualPath(string virtualPath) {
+      if (virtualPath.StartsWith(@"~/"))
+        return virtualPath.Replace(@"~/", string.Empty).TrimStart('/');
 
-			if (virtualPath.StartsWith(@"/upload/"))
-				return virtualPath;
+      if (virtualPath.StartsWith(@"/N2/"))
+        return virtualPath.Replace(@"/N2/", string.Empty).TrimStart('/');
 
-			return virtualPath.TrimStart('/');
-		}
+      if (virtualPath.StartsWith(@"/upload/"))
+        return virtualPath;
 
-		private void DeleteDirectoryAndChildren(string virtualPath) {
-			var directories = GetDirectories(virtualPath);
-			foreach (var directory in directories)
-				DeleteDirectoryAndChildren(directory.VirtualPath);
+      return virtualPath.TrimStart('/');
+    }
 
-			var files = GetFiles(virtualPath);
-			foreach (var file in files)
-				DeleteFile(file.VirtualPath);
+    private void DeleteDirectoryAndChildren(string virtualPath) {
+      var directories = GetDirectories(virtualPath);
+      foreach (var directory in directories)
+        DeleteDirectoryAndChildren(directory.VirtualPath);
 
-			var request = new DeleteObjectRequest()
-				.WithBucketName(_bucketName)
-				.WithKey(virtualPath + EmptyFilename);
-			using (_s3.DeleteObject(request)) { }
-		}
+      var files = GetFiles(virtualPath);
+      foreach (var file in files)
+        DeleteFile(file.VirtualPath);
 
-	}
+      var request = new DeleteObjectRequest()
+        .WithBucketName(_bucketName)
+        .WithKey(virtualPath + EmptyFilename);
+      using (_s3.DeleteObject(request)) { }
+    }
+
+  }
 }
